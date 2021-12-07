3373642281c708449496988a22d09f9c4da1c057
giridhan <giridhanjoshuva@gmail.com>
2021-12-07 16:57:18 +0530

    usernamefunctionality

--- src/org/login/login.java
+++ src/org/login/login.java
@@ -1,5 +1,12 @@
 package org.login;
 
 public class login {
+	public static void userName() {
+    System.out.println("username");
+
+	}
+	public static void main(String[] args) {
+		userName();
+	}
 
 }
